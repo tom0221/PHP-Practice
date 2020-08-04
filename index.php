cac444e27134613c491fc93464713cf600dda231
Tatsuro Muto <tomuto0221@gmail.com>
2020-08-04 14:15:09 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	echo 'こんにちは';
+	echo 'PHPを学ぼう';
+	echo 3+5;
+
+	//変数パターン
+	$fruit = 'りんご';
+	echo $fruit;
+
+	//変数の値を更新
+	$fruit = 'ぶどう';
+	echo $fruit;
+
+	//文字列の連結
+	echo $fruit.'が好きです';
+
+	//文字列の連結パート２
+	$name = 'にんじゃ';
+	$name .='わんこ';
+	echo $name;
+?>
+
