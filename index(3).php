5f89bda43aa21e5e713867cb1d0782a71aa3ed96
Tatsuro Muto <tomuto0221@gmail.com>
2020-08-16 20:43:48 +0900

    データのファイル分け

--- index(3).php
+++ index(3).php
@@ -7,3 +7,14 @@
 
 <!-- <p>タグの中で、$pastaのnameプロパティを表示してください -->
 <p><?php echo $pasta->name ?></p>
+
+<div class="menu-items">
+	<?php foreach ($menus as $menu): ?>
+		<div class="menu-item">
+			<img src="<?php echo $menu->image ?>">
+			<h3 class="menu-item-name"><?php echo $menu->name ?></h3>
+          	<!-- <p>タグの中に、$menuのpriceプロパティを表示 -->
+        	<p class="price">¥<?php echo $menu->price ?></p>
+		</div>
+	<?php endforeach ?>
+</div>
