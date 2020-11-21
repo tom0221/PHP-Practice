b9ce03704cf6f89e1f1c264dca117f3bfdaf2f54
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-21 11:29:51 +0900

    9.switch文

--- index(4).php
+++ index(4).php
@@ -142,5 +142,32 @@
 		}
 	?>
 
+	<?php
+		// 変数$numを定義し、好きな数字を代入してください
+		$num = 5;
+
+		// 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
+		$remainder = $num % 3;
+
+		// switch文を用いてください
+		switch ($remainder) {
+		case 0:
+			echo '大吉です。';
+			break;
+
+		case 1:
+			echo '中吉です。';
+			break;
+
+		case 2:
+			echo '小吉です。';
+			break;
+
+		default:
+			echo '凶です。';
+			break;
+		}
+	?>
+
 </body>
 </html>
